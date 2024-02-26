ec50610c78a84e012e09697659217313d48b8032
Luis Manuel Duarte Bobadilla <luis_duarte@ucne.edu.do>
2024-02-26 14:35:10 -0600

    Hize contexto de Ticket detalle

--- Tickets_DetalleWebAssembly.Api/DAL/Contexto.cs
+++ Tickets_DetalleWebAssembly.Api/DAL/Contexto.cs
@@ -7,5 +7,6 @@ namespace Tickets_DetalleWebAssembly.DAL
     {
         public Contexto(DbContextOptions<Contexto> options) : base(options) { }
         public DbSet<Tickets> Tickets { get; set; }
+        public DbSet<Tickets_Detalle> Tickets_Detalle { get; set; }
     }
 }
