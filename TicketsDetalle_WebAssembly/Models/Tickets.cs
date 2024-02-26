053ef8f4abbf9ee73674928ff65666ddf6e3fda4
Luis Manuel Duarte Bobadilla <luis_duarte@ucne.edu.do>
2024-02-26 07:39:19 -0600

    Cree el modelo de Tickets y de Detalle

---
+++ TicketsDetalle_WebAssembly/Models/Tickets.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicketsDetalle_WebAssembly.Models
+{
+    public class Tickets
+    {
+        [Key]
+        public int TicketId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string? Asunto { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string? Descripcion { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [StringLength(30, ErrorMessage = "No puede exceder los 30 Caracteres")]
+        public string? SolicitadoPor { get; set; }
+    }
+}
---
+++ TicketsDetalle_WebAssembly/Models/Tickets_Detalle.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace TicketsDetalle_WebAssembly.Models
+{
+    public class Tickets_Detalle
+    {
+        
+            [Key]
+            public int DetalleId { get; set; }
+            [Required(ErrorMessage = "Este campo es requerido")]
+            [ForeignKey("TicketId")]
+            public int TicketId { get; set; }
+            [Required(ErrorMessage = "Este campo es requerido")]
+            public string? Emisor { get; set; }
+            [Required(ErrorMessage = "Este campo es requerido")]
+            public string? Mensaje { get; set; }
+    }
+}
